f2a5c8efb587f005f4b6e2f443faeedd99ceead7
Vaibhav Verma <vaibhav2614@gmail.com>
2013-08-09 16:41:15 -0700

    Fabric is amazing

---
+++ fabfile.py
@@ -0,0 +1,17 @@
+from fabric.api import local, env, hosts
+from fabric.operations import run, put
+
+def build():
+    local('jekyll build')
+
+@hosts('vverna@vi.rutgers.edu')
+def ilab():
+    put('_site/*', '/ilab/users/vverna/public_html')
+
+@hosts('vverna@vverma.net')
+def site():
+    put('_site/*', '/var/www/servers/vverma.net')
+
+def deploy():
+    ilab()
+    site()
